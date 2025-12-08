ec540a57720b30038b7f3974cd74bc645b92acae
thduss <kimso1279@naver.com>
2025-12-08 13:56:27 +0900

    Feature: 회원 정보 수정, S3 미디어 업로드 및 로그아웃 API 구현

--- src/main/java/com/trip/aslung/user/model/service/UserServiceImpl.java
+++ src/main/java/com/trip/aslung/user/model/service/UserServiceImpl.java
@@ -1,14 +1,20 @@
 package com.trip.aslung.user.model.service;
 
 import com.trip.aslung.user.model.dto.User;
+import com.trip.aslung.user.model.dto.UserUpdateRequest;
 import com.trip.aslung.user.model.mapper.UserMapper;
+import com.trip.aslung.util.S3Uploader;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
     private final UserMapper userMapper;
+    private final S3Uploader s3Uploader;
     @Override
     public User getUserByEmail(String email) {
         return userMapper.findByEmail(email);
@@ -17,5 +23,21 @@ public class UserServiceImpl implements UserService {
     @Override
     public User findByUserId(Long userId) {
         return userMapper.findByUserId(userId);
+    }
+
+    @Override
+    @Transactional
+    public void updateProfile(Long userId, UserUpdateRequest request, MultipartFile image) {
+        User user = userMapper.findByUserId(userId);
+
+        String profileImageUrl = user.getProfileImageUrl();
+
+        if(image != null && !image.isEmpty()){
+            profileImageUrl = s3Uploader.upload(image, "profile");
+        }
+
+        user.updateProfile(request.getNickname(), profileImageUrl);
+
+        userMapper.updateUser(user);
     }
 }
